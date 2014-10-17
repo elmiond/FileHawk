380302ade775b7e4192404101fdb341d39672325
bondo11 <mbh@bysted.dk>
2014-10-17 12:32:39 +0200

    Clean up imports
    removed som imports

--- src/org/fh/fileUserPermission.java
+++ src/org/fh/fileUserPermission.java
@@ -2,19 +2,14 @@ package org.fh;
 
 import java.io.IOException;
 import java.nio.file.FileSystemNotFoundException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.AclEntry;
 import java.nio.file.attribute.AclEntryPermission;
 import java.nio.file.attribute.AclEntryType;
 import java.nio.file.attribute.AclFileAttributeView;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.UserPrincipal;
-import java.nio.file.attribute.UserPrincipalLookupService;
 import java.nio.file.attribute.UserPrincipalNotFoundException;
-import java.security.acl.AclNotFoundException;
 import java.util.Collections;
 import java.util.EnumSet;
 
